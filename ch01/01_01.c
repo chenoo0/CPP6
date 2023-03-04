091691add139a4c72a9d982f5fd3a49060385980
chenoo0 <1911534298@qq.com>
2023-03-04 18:25:17 +0800

    提交ch01
    源代码

---
+++ ch01/01_01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int dogs;
+
+    printf("How manydogs do you have?\n");
+    scanf("%d", &dogs);
+    printf("so you have %d dog(s)!\n",dogs);
+    
+    return 0;
+}
---
+++ ch01/01_02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (void)
+{
+    printf("Concerte contains gravel and cement.\n");
+
+    return 0;
+}
